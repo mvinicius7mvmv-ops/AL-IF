c5c5bb061bc3be7326ce6b3d75eca06d9ddc7b51
mvinicius7mvmv-ops <mvinicius7.mv.mv@gmail.com>
2026-08-11 00:52:54 -0400

    Updated PlayerLayout.tsx

--- src/screens/player/PlayerLayout.tsx
+++ src/screens/player/PlayerLayout.tsx
@@ -14,6 +14,7 @@ const playerNav = [
   { label: 'Mensalidades', path: '/jogador/mensalidades', icon: Wallet },
   { label: 'Meu Perfil', path: '/jogador/perfil', icon: UserIcon },
   { label: 'Premiações', path: '/jogador/premiacoes', icon: Trophy },
+  { label: 'Cards de Jogos', path: '/jogador/cards', icon: ImageIcon },
 ];
 
 export function PlayerLayout({ children }: { children: React.ReactNode }) {
